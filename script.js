3c0bc570b4db1d7abac2082861a77079731a7535
maicon29 <maiconavelar30@gmail.com>
2023-05-08 21:13:45 -0300

    salv

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert('em produção')
+let show = true;
+
+const conteudo = document.querySelector('.conteudo');
+const toggle = conteudo.querySelector('.toggle');
+
+ toggle.addEventListener('click', () => {
+
+    document.body.style.overflow = show ? 'hidden' : 'initial'
+
+    conteudo.classList.toggle('on', show);
+    show = !show;
+})
+
+alert('em processo de produção')
